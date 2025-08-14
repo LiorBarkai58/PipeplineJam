77f96e4d888ad256f8bf7608622f0d3f3f4bc995
YonatanAmm <yonatan1922@gmail.com>
2025-08-15 02:55:03 +0300

    Add Enemy Hierarchy

--- Assets/Scripts/Enemies/FlyingEnemy.cs
+++ Assets/Scripts/Enemies/FlyingEnemy.cs
@@ -4,22 +4,15 @@ using UnityEngine;
 using UnityEngine.Events;
 using Random = UnityEngine.Random;
 
-public class Enemy : MonoBehaviour
+public class FlyingEnemy : Enemy
 {
-    //Events
-    public event UnityAction OnHitPlayerEvent; 
-    
     //References
     [SerializeField] private EnemyDetection detector;
-    [SerializeField] private BoxCollider2D collider;
     
     //Vars
     [SerializeField] private float targetMinDistance;
     [SerializeField] private float targetMaxDistance;
-    [SerializeField] private float speed;
-    [SerializeField] private float detectionRangeOffset = 0.1f;
     
-    private Vector3 target;
     
     private void Awake()
     {
@@ -52,28 +45,12 @@ public class Enemy : MonoBehaviour
         Debug.Log("Targets Point");
     }
 
-    private Vector3 GetTarget()
+    public override Vector3 GetTarget()
     {
         float radius = Random.Range(targetMinDistance, targetMaxDistance);
         float angle = Random.Range(0, Mathf.PI * 2);
         Vector3 direction = detector.transform.position + new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0) * radius;
         return direction;
-    }
-
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        if (other.gameObject.tag == "Player")
-        {
-            if (collider.bounds.max.y - detectionRangeOffset > other.collider.bounds.min.y)
-            {
-                OnHitPlayerEvent?.Invoke();
-                Debug.Log("Hit Player");
-            }
-            else
-            {
-                Destroy(this.gameObject);
-            }
-        }
     }
 }
 
