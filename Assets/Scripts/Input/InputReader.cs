f0bc8774f0c48b57811a83e6c9c5510cc7cdcaef
Cornetto <lior5858@gmail.com>
2025-08-14 22:46:17 +0300

    Add basic inputreader

---
+++ Assets/Scripts/Input/InputReader.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+using static PlayerInput;
+namespace Input
+{
+    [CreateAssetMenu(fileName = "InputReader", menuName = "Input/InputReader")]
+    public class InputReader : ScriptableObject, PlayerInput.IPlayerActions
+    {
+        public bool isDrawing { get; private set; } = false;
+        
+        public event UnityAction OnPause;
+        public event UnityAction OnDrawFinish;
+        public event UnityAction OnDrawStart;
+        
+
+        private PlayerInput inputActions;
+        
+        void OnEnable() {
+            if (inputActions == null) {
+                inputActions = new PlayerInput();
+                inputActions.Player.SetCallbacks(this);
+            }
+            EnablePlayerActions();
+    
+        }
+
+        void OnDisable()
+        {
+            DisablePlayerActions();
+        }
+
+        public void EnablePlayerActions() {
+            inputActions.Enable();
+        }
+
+        public void DisablePlayerActions() {
+            inputActions.Disable();
+        }
+
+
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            //NOOP
+        }
+
+        public void OnLook(InputAction.CallbackContext context)
+        {
+            //NOOP
+        }
+
+        public void OnAttack(InputAction.CallbackContext context)
+        {
+            //NOOP
+        }
+
+        public void OnCrouch(InputAction.CallbackContext context)
+        {
+            //NOOP
+        }
+
+        public void OnJump(InputAction.CallbackContext context)
+        {
+            //NOOP
+        }
+    }
+}
