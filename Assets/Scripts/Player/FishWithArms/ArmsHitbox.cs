fe6d74c223ab40f0d19b4c102a28b18e53498c22
Cornetto <lior5858@gmail.com>
2025-08-15 19:32:06 +0300

    Start arms

---
+++ Assets/Scripts/Player/FishWithArms/ArmsHitbox.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player.FishWithArms
+{
+    public class ArmsHitbox : MonoBehaviour
+    {
+        [SerializeField] private Collider2D hitbox;
+
+
+        
+    }
+}
