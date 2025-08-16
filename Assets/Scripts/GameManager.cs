50187470f9db30b2e896e364740a795de21211c9
Cornetto <lior5858@gmail.com>
2025-08-16 10:49:24 +0300

    Start working on yonatan's changes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -24,8 +24,6 @@ public class GameManager : MonoBehaviour
     {
         currentLevel = levelManager.GenerateLevel(0);
         fishSwapManager.OnEndLevelEvent += OnEndLevel;
-        fishHealthManager.OnDeathEvent += fishSwapManager.SwapFish;
-        fishHealthManager.OnDeathEvent += fishHealthManager.ResetHp;
         fishHealthManager.OnDeathEvent += PlayerDeath;
         uiManager.UpdateLevel(1);
 
