8e05ed8d545e76d72c301a040928034d8d9f970e
YonatanAmm <yonatan1922@gmail.com>
2025-08-16 03:03:22 +0300

    Add Level Layout

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -7,17 +7,24 @@ public class LevelManager : MonoBehaviour
 {
     //References
     [SerializeField] private List<GameObject> levelSegmentPrefabs;
+    [SerializeField] private GameObject endLevelSegmentPrefab;
     
     //Vars
     [SerializeField] private float gap = 20.48f;
     [SerializeField] private int bgCount = 2;
 
+    private void Awake()
+    {
+        GenerateLevel();
+    }
+
     private void GenerateLevel()
     {
         for (int i = 0; i < levelSegmentPrefabs.Count; i++)
         {
-            Instantiate(levelSegmentPrefabs[i], new Vector3(transform.position.x, transform.position.y + (-gap * 2 * bgCount), 0), transform.rotation);
+            Instantiate(levelSegmentPrefabs[i], new Vector3(transform.position.x, transform.position.y + (-gap* bgCount* i), 0), transform.rotation);
         }
+        Instantiate(endLevelSegmentPrefab, new Vector3(transform.position.x, transform.position.y + (-gap * bgCount* levelSegmentPrefabs.Count), 0), transform.rotation);
     }
 
     private void GenerateLevelRandom()
@@ -27,8 +34,9 @@ public class LevelManager : MonoBehaviour
         {
             int selected = Random.Range(0, copy.Count);
             GameObject temp = copy[selected];
-            Instantiate(temp,new Vector3(transform.position.x, transform.position.y + (-gap * 2 * bgCount), 0), transform.rotation);
+            Instantiate(temp,new Vector3(transform.position.x, transform.position.y + (-gap * bgCount * i), 0), transform.rotation);
             copy.Remove(temp);
         }
+        Instantiate(endLevelSegmentPrefab, new Vector3(transform.position.x, transform.position.y + (-gap * bgCount* (levelSegmentPrefabs.Count + 1)), 0), transform.rotation);
     }
 }
